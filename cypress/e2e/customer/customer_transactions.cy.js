41a2199a413e7fe9d174a7f91960d92149fec800
Toronto22 <toronto.nigma@gmail.com>
2023-05-04 15:43:12 +0700

    Init cypress project

---
+++ cypress/e2e/customer/customer_transactions.cy.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+
+describe('customer transactions test', () => {
+  beforeEach(() => {
+    cy.visit('https://example.cypress.io/todo')
+  })
+
+  it('Customer deposits money with valid amount', () => {
+  })
+
+  it('Customer is unable to withdraw the money that exceed the balance', () => {
+  })
+  
+  it('Customer withdraws money with valid amount', () => {
+  })
+  
+  it('Customer resets the customer transactions', () => {
+  })
+})
+
---
+++ cypress/e2e/customer/viewing_customer_account.cy.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+
+
+describe('viewing customer account test', () => {
+  beforeEach(() => {
+    cy.visit('https://example.cypress.io/todo')
+  })
+
+  it('Customer Views Default Account Information Details', () => {
+  })
+
+  it('Customer Views Other Account Information Details', () => {
+  })
+  
+})
+
