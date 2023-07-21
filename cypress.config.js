5546aab5c57e32bb756259570afdaa251a5c777e
Toronto22 <toronto.nigma@gmail.com>
2023-07-21 21:35:47 +0700

    update github actions

--- cypress.config.js
+++ cypress.config.js
@@ -8,5 +8,9 @@ module.exports = defineConfig({
         setupNodeEvents(on, config) {
             // implement node event listeners here
         },
+        reporter: 'junit',
+        reporterOptions: {
+            mochaFile: 'results/junit.xml',
+        },
     },
 });
