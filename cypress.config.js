bce1f743e635971bee7cf0da4c4803fc7c956bef
Toronto22 <toronto.nigma@gmail.com>
2023-07-21 22:36:40 +0700

    update github actions

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const {defineConfig} = require("cypress");
 module.exports = defineConfig({
     e2e: {
         projectId: "vbf851",
-        defaultCommandTimeout: 15000,
+        defaultCommandTimeout: 5000,
         testIsolation: true,
         setupNodeEvents(on, config) {
             // implement node event listeners here
